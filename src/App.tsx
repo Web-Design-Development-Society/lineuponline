4fb28b2fab1b03fd215625f8b206a6cea400e0d8
alexanderdombroski <dom21013@byui.edu>
2025-05-22 16:52:38 -0600

    Added timer package
    Co-authored-by: ameliarose003 <ameliarose003@users.noreply.github.com>
    Co-authored-by: Abraham Jimenez <abrahamjimenez@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+import Timer from './components/Timer'
+
+type Period = 'Focus' | 'Long Break' | 'Short Break'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cyclesCompleted, setCyclesCompleted] = useState<number>(0)
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [currentPeriod, setCurrentPeriod] = useState<Period>('Focus')
+
+  useEffect(() => {
+    setCyclesCompleted(1)
+  }, [])
+
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <h1>Pomodoro</h1>
+      <p>Cycles Completed: {cyclesCompleted}</p>
+      <Timer></Timer>
     </>
   )
 }
