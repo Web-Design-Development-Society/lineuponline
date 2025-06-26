863239758ad862896d93bde7be26db4b4612f69c
Abraham Jimenez <64721034+abrahamjimenez@users.noreply.github.com>
2025-06-25 18:24:21 -0600

    Fix start and stop buttons

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { CountdownCircleTimer } from 'react-countdown-circle-timer'
 
 export default function Timer() {
+  const [playing, setPlaying] = useState(false)
+
+  const handleStart = () => {
+    setPlaying(true)
+  }
+
+  const handleStop = () => {
+    setPlaying(false)
+  }
+
   return (
     <>
       <CountdownCircleTimer
-        isPlaying
+        isPlaying={playing}
         duration={15 * 60}
         colors={['#004777', '#F7B801', '#A30000', '#A30000']}
         colorsTime={[7, 5, 2, 0]}
@@ -13,8 +23,8 @@ export default function Timer() {
         {(renderProps) => <FormatDisplay {...renderProps} />}
       </CountdownCircleTimer>
       <div>
-        <button>Start</button>
-        <button>Stop</button>
+        <button onClick={handleStart}>Start</button>
+        <button onClick={handleStop}>Stop</button>
       </div>
     </>
   )
